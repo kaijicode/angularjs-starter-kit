d62bfac71c93cb324a3d63b738176be181ba50be
Vladimir Zeifman <edoomik@gmail.com>
2019-01-26 14:50:11 +0200

    Minify CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 
 module.exports = {
@@ -36,7 +37,8 @@ module.exports = {
       new MiniCssExtractPlugin({
          filename: 'app.[contenthash].css',
          chunkFilename: "lib.[contenthash].css"
-      })
+      }),
+      new OptimizeCssAssetsPlugin()
    ],
    module: {
       rules: [
